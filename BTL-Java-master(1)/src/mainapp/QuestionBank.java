0c0197b3442a91d2270ae72fbdf6f25cafe3d677
Mphuc02 <brolydinh@gmail.com>
2022-10-19 23:00:37 +0700

    Update ngay 19/10/2022

--- BTL-Java-master(1)/src/mainapp/QuestionBank.java
+++ BTL-Java-master(1)/src/mainapp/QuestionBank.java
@@ -1,23 +1,26 @@
 package mainapp;
 
-import javax.swing.*;
+import java.awt.*;
 
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.Image;
+import javax.swing.BorderFactory;
+import javax.swing.ButtonGroup;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
 
 public class QuestionBank extends JFrame
 {
     private JPanel panelMain;
 
-    GridBagConstraints gbc;
+    private GridBagConstraints gbc;
 
     public QuestionBank()
     {
-        JLabel lbTitle = new JLabel("Danh sach cau hoi");
-        lbTitle.setFont(new Font("Arial",100,40));
-        this.add(lbTitle);
-
         initFrame();
 
         initmainPanel();
@@ -27,6 +30,11 @@ public class QuestionBank extends JFrame
 
     private void initFrame()
     {
+        JLabel lbTitle = new JLabel("Danh sach cau hoi");
+        lbTitle.setFont(new Font("Arial",100,40));
+        lbTitle.setBounds(350, 0, 1050, 40);
+        this.add(lbTitle);
+
         this.setBackground(Color.CYAN);
         this.setBounds(250, 100, 1050,650);
         this.setResizable(false);
@@ -39,12 +47,14 @@ public class QuestionBank extends JFrame
 
     private void createQuestionPanel(int index)
     {
+        gbc.gridx = 0;
+        gbc.gridy = index;
+
         JPanel panelTemp = new JPanel();
         panelTemp.setLayout(null);
-        panelTemp.setBounds(100, 40 * (index + 1), 500,300);
+        panelTemp.setBounds(40, (index) * 300 + 10 * (index + 1), 620, 300);
 
         JLabel lbQuestionID = new JLabel(String.format("Cau hoi %d:", index + 1)),
-               lbQuestionTitle = new JLabel("Cau hoi"),
                lbAnswer[] = new JLabel [4];
 
         JRadioButton rbtn[] = new JRadioButton [4];
@@ -52,34 +62,56 @@ public class QuestionBank extends JFrame
         JTextField tfQuestionTitle = new JTextField(),
                    tfAnswer [] = new JTextField [4];
 
+        lbQuestionID.setBounds(10,10,70,40);
         panelTemp.add(lbQuestionID);
+
+        ButtonGroup bg = new ButtonGroup();
+
         panelTemp.add(tfQuestionTitle);
+        tfQuestionTitle.setBounds(70,20,300,20);
 
         for(int i = 0 ; i < 4; i++)
         {
-            lbAnswer[i] = new JLabel("A" + i);
+            lbAnswer[i] = new JLabel( (char)('A' + i)  + ":");
+            lbAnswer[i].setBounds(30, 50 * (i + 1),30,20);
+
             rbtn[i] = new JRadioButton();
+            rbtn[i].setBounds(10, 50 * (i + 1),20,20);
+
             tfAnswer[i] = new JTextField();
-            panelTemp.add(lbAnswer[i]);
+            tfAnswer[i].setBounds(50, 50 * (i + 1), 200,20);
+
             panelTemp.add(rbtn[i]);
+            panelTemp.add(lbAnswer[i]);
             panelTemp.add(tfAnswer[i]);
+
+            bg.add(rbtn[i]);
         }
 
-        panelMain.add(panelTemp);
+        JButton btnDeleteQuestion = new JButton("Xoa cau hoi");
+        btnDeleteQuestion.setBounds(500,250,100,30);
+        panelTemp.add(btnDeleteQuestion);
+
+        JLabel lbteJLabel = new JLabel(index + "");
+        panelMain.add(lbteJLabel,gbc);
+
+        panelMain.add(panelTemp, gbc);
     }
 
     private void initmainPanel()
     {
-        gbc = new GridBagConstraints();
+        this.gbc = new GridBagConstraints();
+        
         panelMain = new JPanel();
         panelMain.setOpaque(true);
         panelMain.setBackground(Color.white);
         panelMain.setLayout(new GridBagLayout());
 
-        createQuestionPanel(0);
+        for(int i = 0 ; i < 10; i++)
+            createQuestionPanel(i);
 
-        JScrollPane scrollPane = new JScrollPane(panelMain,JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-        scrollPane.setBounds(175, 0, 700, 650);
+        JScrollPane scrollPane = new JScrollPane(panelMain, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        scrollPane.setBounds(175, 40, 700, 570);
         scrollPane.getVerticalScrollBar().setUnitIncrement(15);
         scrollPane.setBorder(BorderFactory.createLineBorder(Color.BLACK));
 
