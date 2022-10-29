4f47f3730eff6cebf53d68e94abe08a75eba0394
Stargazer131 <naotomori131@gmail.com>
2022-10-29 14:24:30 +0700

    test

--- test.java
+++
@@ -1,9 +0,0 @@
-import java.util.HashMap;
-
-public class test {
-    public static void main(String[] args) {
-        HashMap<Integer,Integer> map = new HashMap<>();
-        map.put(1, 1);
-        System.out.println(map.get(2));
-    }
-}
